15be9331f19bd4c3e4b3d63e80e9470b89577676
Adi Wirya <adi_wirya@student.umn.ac.id>
2026-06-13 00:15:49 +0700

    feat: add HostPanel component

---
+++ src/components/HostPanel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="display: flex; flex-direction: column; gap: 24px">
+    <SharePanel :code="code" />
+    <hr style="border: none; border-top: 1px solid #e5e7eb; margin: 0" />
+    <EntryList :entries="entries" />
+    <hr style="border: none; border-top: 1px solid #e5e7eb; margin: 0" />
+    <SpinLog :log="log" />
+  </div>
+</template>
+
+<script setup>
+import SharePanel from './SharePanel.vue'
+import EntryList from './EntryList.vue'
+import SpinLog from './SpinLog.vue'
+
+defineProps({
+  code: { type: String, required: true },
+  entries: { type: Array, required: true },
+  log: { type: Array, required: true },
+})
+</script>
